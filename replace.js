e5ce403a5dcfd08e6342ba3d5cbce63988649962
Balxkodehodet <balx042025@gmail.com>
2025-05-06 10:03:04 +0200

    newest

---
+++ replace.js
@@ -0,0 +1,9 @@
+let word = "*\n";
+let word2 = "*";
+
+let repeatedWord2 = word2.repeat(120);
+let repeatedWord = word.repeat(10);
+
+console.log(repeatedWord2);
+console.log(repeatedWord);
+console.log(repeatedWord2);
